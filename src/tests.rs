5ee9a92a64c97a28bd17c99a14df2f7030879d88
Jeffrey Yu <wehjin@gmail.com>
2016-09-02 22:39:04 -0700

    Add Frame

--- src/tests.rs
+++ src/tests.rs
@@ -23,3 +23,27 @@ fn shift_y() {
     let cage = Cage::unit_xy().shift_y(-1.0);
     assert_eq!((-1.0, 1.0, -2.0, 0.0, 0.0, 0.0), cage.tuple());
 }
+
+#[test]
+fn default_frame() {
+    let frame: Frame = Default::default();
+    assert_eq!((0.01, 0.01, 0.01), (frame.w, frame.h, frame.d));
+}
+
+#[test]
+fn unit_frame() {
+    let frame = Frame::unit();
+    assert_eq!((0.01, 0.01, 0.01), (frame.w, frame.h, frame.d));
+}
+
+#[test]
+fn zero_frame() {
+    let frame = Frame::zero();
+    assert_eq!((0.0, 0.0, 0.0), (frame.w, frame.h, frame.d));
+}
+
+#[test]
+fn scale_frame() {
+    let frame = Frame { w: 1.0, h: 1.0, d: 1.0 }.scale(1.0, 2.0, 3.0);
+    assert_eq!((1.0, 2.0, 3.0), (frame.w, frame.h, frame.d));
+}
